6622ba6f9163d7cfb24fea0d868e752acd6626d7
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-30 18:24:53 +0530

    Initial Commit

---
+++ simple_chain.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+template = PromptTemplate(
+    template="Generate key 5 facts on {topic}",
+    input_variables=['topic']
+)
+
+user_input = input("Enter the topic ")
+
+# prompt = template.invoke({'topic': user_input})
+
+# result = model.invoke(prompt)
+
+# print(result.content)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic': user_input})
+
+print(result)
+
